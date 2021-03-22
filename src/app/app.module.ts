f13a5ee6283897789b3d3f79a1425bd94fe4899a
RominaURR <rominaurr@gmail.com>
2021-03-22 14:51:19 -0600

    heroku changes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,26 @@
-import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
+
+import { HttpClientModule } from '@angular/common/http';
+
+import { UsuariosService } from './usuarios.service';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    HttpClientModule
   ],
-  providers: [],
+  providers: [
+    UsuariosService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
